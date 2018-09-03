36302c8a5705d2a6276000c10303baacfc8e1200
Ineededsometext <38172273+Ineededsometext@users.noreply.github.com>
2018-09-03 21:39:46 +0800

    Bug fixes.

--- Invisibilty Cloak/lua/autorun/client/cl_ced_cloak.lua
+++ Invisibilty Cloak/lua/autorun/client/cl_ced_cloak.lua
@@ -1,20 +1,19 @@
-
-local CedCloakLerp = 0
+LocalPlayer():SetNWInt("CedCloakLerp", 0)
 
 hook.Add("HUDPaint", "Screen Effect", function()
     if LocalPlayer():Alive() then
         if LocalPlayer():GetNWBool("UsingInvisibilityCloak") == true then
-            CedCloakLerp = Lerp(5 * FrameTime(), CedCloakLerp, ScrH())
+            LocalPlayer():SetNWInt("CedCloakLerp", Lerp(5 * FrameTime(), LocalPlayer():GetNWInt("CedCloakLerp"), ScrH()))
             surface.SetMaterial(Material("vgui/gradient_down"))
             surface.SetDrawColor(Color(50, 50, 100, 50))
-            surface.DrawTexturedRect(0, 0, ScrW(), CedCloakLerp)
+            surface.DrawTexturedRect(0, 0, ScrW(), LocalPlayer():GetNWInt("CedCloakLerp"))
             
             DrawMotionBlur(0.4, 0.8, 0.01)
         else
-            CedCloakLerp = Lerp(5 * FrameTime(), CedCloakLerp, 0)
+            LocalPlayer():SetNWInt("CedCloakLerp", Lerp(5 * FrameTime(), LocalPlayer():GetNWInt("CedCloakLerp"), 0))
             surface.SetMaterial(Material("vgui/gradient_down"))
             surface.SetDrawColor(Color(50, 50, 100, 50))
-            surface.DrawTexturedRect(0, 0, ScrW(), CedCloakLerp)
+            surface.DrawTexturedRect(0, 0, ScrW(), LocalPlayer():GetNWInt("CedCloakLerp"))
         end
     end
 end)
