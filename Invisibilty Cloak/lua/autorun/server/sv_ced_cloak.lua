0497f394aaf82925c3e8580859b9385872385bea
Ineededsometext <38172273+Ineededsometext@users.noreply.github.com>
2018-09-03 21:40:32 +0800

    Made variable names unique.

--- Invisibilty Cloak/lua/autorun/server/sv_ced_cloak.lua
+++ Invisibilty Cloak/lua/autorun/server/sv_ced_cloak.lua
@@ -1,12 +1,12 @@
 local TTT = gmod.GetGamemode().Name == "Trouble in Terrorist Town"
 
-hook.Add("PlayerSpawn", "OldColor", function(ply)
-    ply:SetColor(ply.OldColor)
+hook.Add("PlayerSpawn", "Player Variables", function(ply)
+    ply:SetColor(ply.InviCloakOldColor)
     ply:SetMaterial("")
-    ply.OldMaterial = ply:GetMaterial()
+    ply.InviCloakOldMaterial = ply:GetMaterial()
 end)
 
-hook.Add("PlayerDeath", "On player death", function(ply, wep, k)
+hook.Add("PlayerDeath", "Disable cloak on player death", function(ply, wep, k)
     if ply:GetNWBool("UsingInvisibilityCloak") == true then
         ply:SetNWBool("UsingInvisibilityCloak", false)
     end
@@ -19,25 +19,20 @@ hook.Add("EntityTakeDamage", "Damage type while cloaked", function(t, dmg)
 end)
 
 hook.Add("PlayerSwitchWeapon", "Un-cloak on weapon switch", function(ply, old, new)
-    if IsValid(old) and IsValid(ply) and old:GetClass() == "invisibility_cloak" and ply:GetNWBool("UsingInvisibilityCloak") == true then
+    if IsValid(old) and old:GetClass() == "invisibility_cloak" and ply:GetNWBool("UsingInvisibilityCloak") == true then
+        ply:EmitSound("ced/cloak/ced_cloak_disabled.mp3")
         ply:SetNWBool("UsingInvisibilityCloak", false)
-        ply:GetActiveWeapon():EmitSound("ced/cloak/ced_cloak_disabled.mp3")
         ply:SetNoTarget(false)
         ply:SetMaterial("")
 
-        ply:SetColor(ply.OldColor)
+        ply:SetColor(Color(255, 255, 255, 255))
     end
 end)
 
 hook.Add("Think", "Disable cloak if the player doesn't have the SWEP", function()
     for i, v in pairs(player.GetAll()) do
-        if not IsValid(v:GetActiveWeapon()) or v:GetActiveWeapon():GetClass() != "invisibility_cloak" then
+        if not v:HasWeapon("invisibility_cloak") then
             v:SetNWBool("UsingInvisibilityCloak", false)
-        end
-
-        if v:GetNWBool("UsingInvisibilityCloak") == false then
-            v:SetMaterial(v.OldMaterial)
-            v:SetNoTarget(false)
         end
     end
 end)
